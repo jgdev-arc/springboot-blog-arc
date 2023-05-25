9e754cdf4c920e31f2c028e1b9e9eeca9407c172
TheLordZedd <pcgpod@yahoo.com>
2023-05-24 21:47:38 -0500

    Initial commit

---
+++ src/main/java/com/arc/springbootblogarc/SpringbootBlogArcApplication.java
@@ -0,0 +1,13 @@
+package com.arc.springbootblogarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBlogArcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBlogArcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arc/springbootblogarc/SpringbootBlogArcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arc.springbootblogarc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBlogArcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
